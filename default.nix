4b8aff36b1382302f4b6e5b8187276b1c0e12885
Hongrui Fang <chfanghr@gmail.com>
2022-08-07 22:21:36 +0800

    nixify the project

---
+++ default.nix
@@ -0,0 +1,29 @@
+{ lib
+, llvmPackages_11
+, cmake
+, spdlog
+, abseil-cpp }:
+
+llvmPackages_11.stdenv.mkDerivation rec {
+  pname = "embryo";
+  version = "0.1.0";
+  
+  src = ./.;
+
+  nativeBuildInputs = [ cmake ];
+  buildInputs = [ fmt docopt_cpp ];
+
+  cmakeFlags = [
+    "-DENABLE_TESTING=OFF"
+    "-DENABLE_INSTALL=ON"
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/chfanghr/embryo";
+    description = ''
+      Yet another lua compiler/vm, written in C++17.";
+    '';
+    licencse = licenses.mit;
+    platforms = with platforms; linux ++ darwin;
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "embryo: yet another lua compiler/vm";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+
+    utils.url = "github:numtide/flake-utils";
+    utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, utils, ... }@inputs:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShell = pkgs.mkShell rec {
+          name = "embryo";
+
+          packages = with pkgs; [
+            # Development Tools
+            llvmPackages_11.clang
+            cmake
+            cmakeCurses
+
+            # Development time dependencies
+            gtest
+
+            # Build time and Run time dependencies
+            fmt
+            docopt_cpp
+          ];
+
+        };
+
+        defaultPackage = pkgs.callPackage ./default.nix { };
+      });
+}
